df18b39e9ea844e1ce3a0ad4939087a68948ec77
ririelen <elena451.1984@gmail.com>
2026-09-23 18:54:18 +0000

    adding the source files

---
+++ hw4-compilation/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main() {
+	int x = 10;
+	int y = square(x);
+	printf("%d^2 = %d", x, y);
+}
---
+++ hw4-compilation/math_utils.c
@@ -0,0 +1,5 @@
+#include "math_utils.h"
+
+int square(int n) {
+	return n * n;
+}
